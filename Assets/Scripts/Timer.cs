0c2f7b3279252e89ac3f07b2fe61c3f943b00273
vahid-p <dai.shaozhang@gmail.com>
2025-05-15 16:46:10 +1000

    Final Experiment Version

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,59 @@
+using Microsoft.MixedReality.Toolkit.UI;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.UI.BodyUI;
+
+public class Timer : MonoBehaviour
+{
+    public float explorationTime = 360f;
+    private float timer;
+    public GameObject dialogPrefab;
+    private bool isCounting = false;
+    public GameObject handMenu;
+    public GameObject clickableObj;
+
+
+    public MRTKSceneTransition sceneTransition;
+
+    void Update()
+    {
+        if (!isCounting) return;
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            isCounting = false;
+            ShowDialogue();
+
+        }
+    }
+
+    public void StartCountdown()
+    {
+        timer = explorationTime;
+        isCounting = true;
+    }
+
+    public void ShowDialogue()
+    {
+        Dialog myDialog = Dialog.Open(dialogPrefab);
+        if (myDialog != null)
+        {
+            myDialog.OnClosed += result =>
+            {
+                switch (result.Result)
+                {
+                    case DialogButtonType.Yes:
+                        if (sceneTransition != null){
+                            sceneTransition.BeginTransition();
+                        }
+                        handMenu.SetActive(false);
+                        clickableObj.SetActive(false);
+                        Application.Quit();
+                        break;
+                }
+            };
+
+        }
+    }
+}
